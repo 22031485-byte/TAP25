4e5cdd05c170789fe91b4512fec402b88c8a146d
leonardo99gg <lemaolv04@gmail.com>
2025-09-03 01:36:52 -0600

    Proyecto cajero terminado

---
+++ src/Controller/CajeroController.java
@@ -0,0 +1,100 @@
+package Controller;
+
+import Models.CajeroModel;
+import Views.CajeroView;
+
+public class CajeroController {
+   private CajeroModel model;
+   private CajeroView view;
+   private boolean sistemaActivo;
+
+
+   public CajeroController(CajeroModel model, CajeroView view){
+    this.model = model;
+    this.view = view;
+    this.sistemaActivo = true;
+   }
+
+   public void iniciarSistema(){
+    view.mostrarBienvenida();
+    while (sistemaActivo) {
+        if (autenticarUsuario()) {
+            ejecutarMenuPrincipal();
+        }else{
+            view.mostrarMensaje("Credenciales incorrectas ");
+        }
+    }
+    view.mostrarMensaje("Gracias por usar nuestro cajero ");
+   }
+
+   private boolean autenticarUsuario(){
+    String numeroCuenta = view.solicitarNumeroCuenta();
+    String pin = view.solcitarPin();
+    return model.autenticar(numeroCuenta, pin);
+   }
+
+   private void ejecutarMenuPrincipal(){
+     boolean sessionActiva = true;
+     while (sessionActiva) {
+        view.mostrarMenuPrincipal(model.getCuentaActual().getTitular());
+        int opcion = view.leerOpcion();
+        switch (opcion) {
+            case 1:
+            consultarSaldo();
+                
+            break;
+
+            case 2:
+            this.realizarRetiro();
+            break;
+
+            case 3:
+            this.realizarDeposito();
+            break;
+
+            case 4:
+            this.cerrarSistema();
+            break;
+        
+            default:
+            break;
+        }
+     }
+ }
+
+ public void consultarSaldo(){
+    double saldo = model.consultarSaldo();
+    view.mostrarSaldo(saldo);
+ }
+
+ public void realizarRetiro(){
+    double cantidad = view.solicitarCantidad("Retirar");
+    if (cantidad <= 0) {
+        view.mostrarMensaje("Error en la cantidad ");
+        return;
+    }
+
+    if (model.realizarRetiro(cantidad)) {
+        view.mostrarMensaje("Retiro exitoso de "+cantidad);
+    }else{
+        view.mostrarMensaje("Fondos insuficientes ");
+    }
+  }
+
+ public void realizarDeposito(){
+    double cantidad = view.solicitarCantidad("Deposito ");
+    if (cantidad <= 0) {
+        view.mostrarMensaje("Error en la cantidad ");
+        return;
+    }
+    if (model.realizarDeposito(cantidad)) {
+        view.mostrarMensaje("Deposito exitoso por la cantidad de "+cantidad);
+    }else{
+        view.mostrarMensaje("Fondos insuficientes ");
+    }
+ }
+ 
+ public void cerrarSistema(){
+    view.cerrar();
+ }
+}
